f740832581691f13734500d78f7a6ada031f5ff0
Dev-Pratham <pratham.chaurasia31@gmail.com>
2025-07-27 01:42:34 +0530

    intermediate logic for n queens

--- BackTracking.java
+++ BackTracking.java
@@ -50,11 +50,41 @@ public class BackTracking {
 
     }
 
+    public static void printBorard(char board[][]) {
+
+        System.out.println("-----Chess Board-----");
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void nQueens(char board[][], int row) {
+
+        if (row == board.length) {
+            printBorard(board);
+            return;
+        }
+
+        for (int j = 0; j < board.length; j++) {
+            board[row][j] = 'Q';
+            nQueens(board, row + 1);
+            board[row][j] = 'x';
+        }
+
+    }
+
     public static void main(String args[]) {
 
-        String str = "abc";
-        // findSubset(str, "", 0);
-        permutation(str, "");
+        // String str = "abc";
+        // // findSubset(str, "", 0);
+        // permutation(str, "");
+
+        int n = 2;
+        char board[][] = new char[n][n];
+        nQueens(board, 0);
 
     }
 }
