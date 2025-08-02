e04bcaf644bac877462e074074a4dbe3d8d3da2c
Dev-Pratham <pratham.chaurasia31@gmail.com>
2025-08-03 00:07:14 +0530

    solved rate maze problem

--- BackTracking2.java
+++ BackTracking2.java
@@ -1,55 +1,55 @@
 public class BackTracking2 {
-
-    public static void printMaze(int maze[][]) {
-        for (int i = 0; i < maze.length; i++) {
-            for (int j = 0; j < maze.length; j++) {
-                System.out.print(maze[i][j] + " ");
+    public static void printSolution(int sol[][]) {
+        for (int i = 0; i < sol.length; i++) {
+            for (int j = 0; j < sol.length; j++) {
+                System.out.print(" " + sol[i][j] + " ");
             }
             System.out.println();
         }
     }
 
-    public static void recur(int iniMaze[][], int solMaze[][], int i, int j) {
+    public static boolean isSafe(int maze[][], int x, int y) {
+        // if (x, y outside maze) return false
+        return (x >= 0 && x < maze.length
+                && y >= 0 && y < maze.length && maze[x][y] == 1);
+    }
 
-        int n = iniMaze.length;
-        if (i == n || j == n || i == -1 || j == -1) {
-            // The position is out of matrix we simply return
-            return;
+    public static void solveMaze(int maze[][]) {
+        int N = maze.length;
+        int sol[][] = new int[N][N];
+        if (solveMazeUtil(maze, 0, 0, sol) == false) {
+            System.out.print("Solution doesn't exist");
         }
-
-        if (iniMaze[i][j] != 0) {
-
-            solMaze[i][j] = 1;
-        }
-        if (i == n - 1 && j == n - 1) {
-            printMaze(solMaze);
-            System.out.println("-------------------");
-        }
-
-        recur(iniMaze, solMaze, i + 1, j);
-        recur(iniMaze, solMaze, i, j + 1);
-        solMaze[i][j] = 0;
+        printSolution(sol);
 
     }
 
-    public static void main(String args[]) {
+    public static boolean solveMazeUtil(int maze[][], int x, int y, int sol[][]) {
+        if (x == maze.length - 1 && y == maze.length - 1 && maze[x][y] == 1) {
+            sol[x][y] = 1;
+            return true;
+        }
+        // Check if maze[x][y] is valid
+        if (isSafe(maze, x, y) == true) {
+            if (sol[x][y] == 1)
+                return false;
+            sol[x][y] = 1;
+            if (solveMazeUtil(maze, x + 1, y, sol))
+                return true;
+            if (solveMazeUtil(maze, x, y + 1, sol))
+                return true;
 
+            sol[x][y] = 0;
+            return false;
+        }
+        return false;
+    }
+
+    public static void main(String args[]) {
         int maze[][] = { { 1, 0, 0, 0 },
                 { 1, 1, 0, 1 },
                 { 0, 1, 0, 0 },
                 { 1, 1, 1, 1 } };
-
-        int mazeSize = maze.length;
-
-        // creating solution maze
-        int solMaze[][] = new int[mazeSize][mazeSize];
-        // initializing solution maze
-        for (int i = 0; i < solMaze.length; i++) {
-            for (int j = 0; j < solMaze.length; j++) {
-                solMaze[i][j] = 0;
-            }
-        }
-
-        recur(maze, solMaze, 0, 0);
+        solveMaze(maze);
     }
 }
