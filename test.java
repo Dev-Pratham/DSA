ef7e80f93deeb0d8a4e69d826dd7ccb0208862e9
Dev-Pratham <pratham.chaurasia31@gmail.com>
2025-08-04 15:36:47 +0530

    tried solving telephone mapper

--- test.java
+++ test.java
@@ -1,85 +1,86 @@
 public class test {
 
-    public static void printmaze(int[][] sol) {
-        for (int i = 0; i < sol.length; i++) {
-            for (int j = 0; j < sol.length; j++) {
-                System.out.print(sol[i][j] + " ");
-            }
-            System.out.println();
+    public static String getCurrentString(char number) {
+        char ch = number;
+        String str = "";
+
+        switch (ch) {
+            case '2':
+                str = "abc";
+
+                break;
+            case '3':
+                str = "def";
+
+                break;
+            case '4':
+                str = "hij";
+
+                break;
+            case '5':
+                str = "klm";
+
+                break;
+            case '6':
+                str = "nop";
+
+                break;
+            case '7':
+                str = "qrs";
+
+                break;
+            case '8':
+                str = "tuv";
+
+                break;
+            case '9':
+                str = "wxyz";
+
+                break;
+
+            default:
+                break;
         }
-        System.out.println();
+
+        return str;
     }
 
-    public static void solveMazeUntil(int maze[][], int x, int y, int sol[][]) {
-        // base case
-        // we reached at the end of the maze
+    public static String getinputString(String number) {
 
-        int n = maze.length;
-        if (x == n - 1 && y == n - 1 && maze[x][y] == 1) {
-            sol[x][y] = 1;
-            printmaze(sol);
-            sol[x][y] = 0;
-            return;
+        String str = "";
+
+        for (int i = 0; i < number.length(); i++) {
+            char ch = number.charAt(i);
+            str = str + getCurrentString(ch);
         }
 
-        // Check if current cell is invalid
-        if (x < 0 || x >= n || y < 0 || y >= n || maze[x][y] == 0 || sol[x][y] == 1) {
-            return;
-        }
-
-        // Mark current cell in solution
-        sol[x][y] = 1;
-
-        // Explore all 4 directions: Down, Left, Right, Up
-        solveMazeUntil(maze, x + 1, y, sol); // Down
-        solveMazeUntil(maze, x, y - 1, sol); // Left
-        solveMazeUntil(maze, x, y + 1, sol); // Right
-        solveMazeUntil(maze, x - 1, y, sol); // Up
-
-        // Backtrack
-        sol[x][y] = 0;
+        return str;
 
     }
 
-    public static void solveMaze(int maze[][]) {
-        int n = maze.length;
-        int sol[][] = new int[n][n];
+    public static int getDigitLength(String str) {
+        return str.length();
+    }
 
-        if (maze[0][0] == 0) {
-            System.out.println("No path possible from start.");
-            return;
+    public static void printString(String str) {
+
+        for (int i = 0; i < str.length(); i++) {
+            System.out.print(str.charAt(i) + " ");
+
         }
-        solveMazeUntil(maze, 0, 0, sol);
 
+    }
+
+    public static void printPermutation(String str) {
+
+        String input = getinputString(str);
+        printString(input);
     }
 
     public static void main(String args[]) {
 
-        // int sudoku2[][] = { { 0, 0, 8, 0, 0, 0, 0, 0, 0 },
-        // { 4, 9, 0, 1, 5, 7, 0, 0, 2 },
-        // { 0, 0, 3, 0, 0, 4, 1, 9, 0 },
-        // { 1, 8, 5, 0, 6, 0, 0, 2, 0 },
-        // { 0, 0, 0, 0, 2, 0, 0, 6, 0 },
-        // { 9, 6, 0, 4, 0, 5, 3, 0, 0 },
-        // { 0, 3, 0, 0, 7, 2, 0, 0, 4 },
-        // { 0, 4, 9, 0, 3, 0, 0, 5, 7 },
-        // { 8, 2, 7, 0, 0, 9, 0, 1, 3 } };
-
-        // if (sudoku(sudoku2, 0, 0)) {
-        // System.out.println("solution exists");
-        // printSudoku(sudoku2);
-        // } else {
-        // System.out.println("Solution does not exists");
-        // }
-
-        int[][] maze = {
-                { 1, 0, 0, 0 },
-                { 1, 1, 0, 1 },
-                { 0, 1, 0, 0 },
-                { 1, 1, 1, 1 }
-        };
-
-        solveMaze(maze);
+        String digit = "23";
+        printPermutation(digit);
 
     }
 }
