371cb2c63d1f60a8a20f11f2876d482d00f2cb10
Dev-Pratham <pratham.chaurasia31@gmail.com>
2025-08-20 23:02:22 +0530

    traversed through multidimension arrayList

--- ArrayListPart1.java
+++ ArrayListPart1.java
@@ -10,8 +10,28 @@ public class ArrayListPart1 {
           list.add(2);
           list.add(3);
 
+          ArrayList<Integer> list2 = new ArrayList<>();
+          list2.add(1);
+          list2.add(2);
+          list2.add(3);
+
           mainlist.add(0, list);
+          mainlist.add(1, list2);
           System.out.println(mainlist);
+
+          // for printing individual elements
+
+          for (int i = 0; i < mainlist.size(); i++) {
+
+               ArrayList<Integer> currentlist = mainlist.get(i);
+
+               for (int j = 0; j < currentlist.size(); j++) {
+
+                    System.out.print(currentlist.get(j));
+               }
+
+               System.out.println();
+          }
 
      }
 }
