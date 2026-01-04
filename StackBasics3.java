94408a18d25fc1c33cb9e4a87c59eac45f9c3f0f
Dev-Pratham <pratham.chaurasia31@gmail.com>
2026-01-05 01:06:38 +0545

    tried solving next element problem

--- StackBasics3.java
+++ StackBasics3.java
@@ -98,6 +98,41 @@ public class StackBasics3 {
 
     }
 
+    public static int[] nxtGreaterElement(int arr[]) {
+
+        int out[] = new int[arr.length];
+        Stack<Integer> s = new Stack<>();
+        s.push(0);
+        int j = 0;
+        int k;
+
+        for (int i = 1; i < arr.length; i++) {
+            k = i;
+            while (s.peek() != arr.length - 1) {
+                s.push(k);
+                if (arr[s.peek()] > arr[j]) {
+                    out[j] = arr[s.peek()];
+                    j++;
+                    break;
+                } else {
+                    k++;
+                }
+            }
+            if (s.peek() == arr.length - 1) {
+                out[j] = -1;
+                j++;
+            }
+        }
+
+        return out;
+    }
+
+    public static void prinArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
     public static void main(String args[]) {
 
         // Stack<Integer> s = new Stack<>();
@@ -126,11 +161,16 @@ public class StackBasics3 {
         // System.out.println(s);
 
         // stock span problem
-        int stocks[] = { 100, 80, 60, 70, 60, 85, 100 };
-        int res[] = stockSpan(stocks);
-        for (int i = 0; i < res.length; i++) {
-            System.out.print(res[i] + " ");
-        }
+        // int stocks[] = { 100, 80, 60, 70, 60, 85, 100 };
+        // int res[] = stockSpan(stocks);
+        // for (int i = 0; i < res.length; i++) {
+        // System.out.print(res[i] + " ");
+        // }
+
+        // next greater element problem
+        int arr[] = { 6, 8, 0, 1, 3 };
+        int res[] = nxtGreaterElement(arr);
+        prinArr(res);
 
     }
 }
