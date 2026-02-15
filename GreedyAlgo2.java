27873d991a544118a1a57d8948a321c9b7d1682f
Dev-Pratham <pratham.chaurasia31@gmail.com>
2026-02-15 19:03:59 +0530

    solved hard greedy chocolatey problem

--- GreedyAlgo2.java
+++ GreedyAlgo2.java
@@ -40,7 +40,7 @@ public class GreedyAlgo2 {
         }
 
         for (int i = 0; i < ans.size(); i++) {
-            System.out.print("Job" + (ans.get(i) + 1));
+            System.out.print("Job" + (ans.get(i) + 1) + " ");
         }
 
         System.out.println();
@@ -48,28 +48,95 @@ public class GreedyAlgo2 {
         return maxprofit;
     }
 
-    public static void main(String args[]) {
+    public static void sort(int arr[]) {
 
-        // job sequencing problem
-        int[][] jobs = {
-                { 2, 100 },
-                { 2, 90 },
-                { 1, 80 }
-        };
+        Arrays.sort(arr);
 
-        Jobs j[] = new Jobs[jobs.length];
+        int start = 0;
+        int end = arr.length - 1;
+        int temp;
 
-        // initialize the object array
-        // jobs[i][0] this is deadline
-        // jobs[i][1] this is profit
-        // i is simply the id of the job A,B,C
-
-        for (int i = 0; i < j.length; i++) {
-            j[i] = new Jobs(i, jobs[i][0], jobs[i][1]);
+        while (start < end) {
+            temp = arr[end];
+            arr[end] = arr[start];
+            arr[start] = temp;
+            start++;
+            end--;
         }
 
-        int maxProfit = jobSequencing(j);
-        System.out.println(maxProfit);
+    }
+
+    public static int chocola(int hCost[], int vCost[]) {
+
+        // sort the array in descending order
+        sort(vCost);
+        sort(hCost);
+
+        int h = 0;
+        int v = 0;
+        int hPiece = 1;
+        int vPiece = 1;
+        int minCost = 0;
+
+        while (h < hCost.length && v < vCost.length) {
+
+            if (hCost[h] >= vCost[v]) {
+                // calculate horizontal cost and increment horizontal piece as well as increment
+                // that pointer
+                minCost += vPiece * hCost[h];
+                hPiece++;
+                h++;
+            } else {
+                minCost += hPiece * vCost[v];
+                vPiece++;
+                v++;
+            }
+        }
+
+        while (h < hCost.length) {
+            minCost += vPiece * hCost[h];
+            hPiece++;
+            h++;
+        }
+
+        while (v < vCost.length) {
+            minCost += hPiece * vCost[v];
+            vPiece++;
+            v++;
+        }
+
+        return minCost;
+    }
+
+    public static void main(String args[]) {
+
+        // // job sequencing problem
+        // int[][] jobs = {
+        // { 4, 20 },
+        // { 1, 10 },
+        // { 1, 40 },
+        // { 1, 30 }
+        // };
+
+        // Jobs j[] = new Jobs[jobs.length];
+
+        // // initialize the object array
+        // // jobs[i][0] this is deadline
+        // // jobs[i][1] this is profit
+        // // i is simply the id of the job A,B,C
+
+        // for (int i = 0; i < j.length; i++) {
+        // j[i] = new Jobs(i, jobs[i][0], jobs[i][1]);
+        // }
+
+        // int maxProfit = jobSequencing(j);
+        // System.out.println(maxProfit);
+
+        // chocola problem
+        int horizontalCost[] = { 4, 1, 2 };
+        int verticalCost[] = { 2, 1, 3, 1, 4 };
+        int minCost = chocola(horizontalCost, verticalCost);
+        System.out.println(minCost);
 
     }
 }
