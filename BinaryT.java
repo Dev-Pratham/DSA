f34b37f207474f91875d4961f632e1e9a2caf4e4
Dev-Pratham <pratham.chaurasia31@gmail.com>
2026-02-19 00:45:37 +0530

    built binary tree

---
+++ BinaryT.java
@@ -0,0 +1,43 @@
+public class BinaryT {
+
+    public static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static class BinaryTree {
+        // this index tracks the index of the given arrat of node
+        static int index = -1;
+
+        public static Node buildTree(int node[]) {
+            index++;
+
+            if (node[index] == -1) {
+                return null;
+            }
+
+            Node newNode = new Node(node[index]);
+            newNode.left = buildTree(node);
+            newNode.right = buildTree(node);
+
+            return newNode;
+
+        }
+    }
+
+    public static void main(String args[]) {
+
+        int node[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        Node root = BinaryTree.buildTree(node);
+        System.out.println(root.data);
+        System.out.println(root.left.data);
+        System.out.println(root.right.data);
+    }
+}
